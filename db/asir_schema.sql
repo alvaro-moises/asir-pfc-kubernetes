11365cda342dc3c24ac392ac8ab4724963e248fc
alvaro-moises <moisesalvaro2023@gmail.com>
2025-06-08 21:48:49 +0200

    Update asir_schema.sql

--- db/asir_schema.sql
+++ db/asir_schema.sql
@@ -19,7 +19,7 @@ CREATE TABLE Proyectos (
     id_proyecto INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
     descripcion TEXT,
-    fecha_creacion DATE DEFAULT NOT NULL,
+    fecha_creacion DATE NOT NULL,
     estado VARCHAR(50) DEFAULT 'activo'
 ) ENGINE=InnoDB;
 
